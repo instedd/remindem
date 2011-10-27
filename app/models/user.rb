892b4176a2760260157c1356c2731087761f48ee
Brian J. Cardiff <bcardiff@gmail.com>
2011-10-27 13:21:37 -0300

    prevent changes in user's email

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,6 +4,12 @@ class User < ActiveRecord::Base
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable, :confirmable
 
+  validate do
+    if !self.new_record? && self.email_changed?
+      errors.add(:email, "can't be changed")
+    end
+  end
+
   # Setup accessible (or protected) attributes for your model
   attr_accessible :email, :password, :password_confirmation, :remember_me
   
--- config/environments/development.rb
+++ config/environments/development.rb
@@ -15,8 +15,7 @@ RememberMe::Application.configure do
   config.action_controller.perform_caching = false
 
   # Don't care if the mailer can't send
-  # config.action_mailer.raise_delivery_errors = false
-  config.action_mailer.raise_delivery_errors = true
+  config.action_mailer.raise_delivery_errors = false
   
   # Print deprecation notices to the Rails logger
   config.active_support.deprecation = :log
@@ -24,10 +23,6 @@ RememberMe::Application.configure do
   # Only use best-standards-support built into browsers
   config.action_dispatch.best_standards_support = :builtin
 
-  config.action_mailer.delivery_method = :smtp
-  config.action_mailer.smtp_settings = {
-    :address              => "esparta.manas.net"
-  }
   config.action_mailer.default_url_options = { :host => 'localhost:3000' }
 end
 
