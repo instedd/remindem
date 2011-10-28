06bc0b296be38865574668c98c1fe2b4c90c5530
Brian J. Cardiff <bcardiff@gmail.com>
2011-10-28 13:21:16 -0300

    update homepage style. add body class helper

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,4 +3,12 @@ class ApplicationController < ActionController::Base
   def after_sign_in_path_for user
   	schedules_path
   end
+  
+  private
+
+  def add_body_class(new_class)
+    @body_class ||= []
+    @body_class << new_class
+  end
+  
 end
--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,2 +1,5 @@
 class HomeController < ApplicationController
+  def index
+    add_body_class 'homepage'
+  end
 end
