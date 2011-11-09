aef576e5e05dc3c7b176ee602e69a4a827260135
nekron <marianoabelcoca@gmail.com>
2011-11-09 18:14:48 -0300

    extracted tour navigation to use a generic helper with a given list of steps

--- app/controllers/tour_controller.rb
+++ app/controllers/tour_controller.rb
@@ -1,9 +1,21 @@
 class TourController < ApplicationController
+  layout 'tour'
+  helper_method :steps
+
   def index
     redirect_to tour_path(:start)
   end
   
   def show
+    @selected_step = params[:page]
     render params[:page]
   end
+
+  def steps
+    { start: 'through any internet connection',
+      schedule: 'Messages in advance',
+      manage: 'as many lists as you want',
+      send: 'your scheduled text messages' }
+  end
+
 end
