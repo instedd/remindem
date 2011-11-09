d01a633c275515530f040a761b1d429f5152be64
nekron <marianoabelcoca@gmail.com>
2011-11-09 15:32:03 -0300

    Added tour navigation. Added configuration for version name.

--- config/initializers/instedd_rails_config.rb
+++ config/initializers/instedd_rails_config.rb
@@ -2,4 +2,5 @@ InsteddRails.configure do |config|
   # config.application_name = ::Rails.application.class.parent_name
   config.application_name = "remindem"
   # google_analytics = ::Rails.application.class.config.google_analytics
+  # self.version_name = ::Rails.application.class.config.version_name
 end
