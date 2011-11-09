f8368a6ecbaf91597ba3b2fb5abb8de82d9084c2
nekron <marianoabelcoca@gmail.com>
2011-11-09 13:38:31 -0300

    Added configuration to override application name

---
+++ config/initializers/instedd_rails_config.rb
@@ -0,0 +1,5 @@
+InsteddRails.configure do |config|
+  # config.application_name = ::Rails.application.class.parent_name
+  config.application_name = "remindem"
+  # google_analytics = ::Rails.application.class.config.google_analytics
+end
