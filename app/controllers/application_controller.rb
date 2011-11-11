232e646770ad8c6aa37085b17208e9738998ec8e
nekron <marianoabelcoca@gmail.com>
2011-11-11 18:11:15 -0300

    removed unnecessary RoutingError rescue in ApplicationController

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,22 +1,10 @@
 class ApplicationController < ActionController::Base
 
-  rescue_from ActionController::RoutingError, :with => :render_404
-  
   protect_from_forgery
 
   def after_sign_in_path_for user
   	schedules_path
   end
-  
-  def render_404
-    respond_to do |type| 
-      type.html { render :template => "errors/error_404", :status => 404, :layout => 'application' } 
-      type.all  { render :nothing => true, :status => 404 } 
-    end
-    true
-  end
-
-
 
   private
 
