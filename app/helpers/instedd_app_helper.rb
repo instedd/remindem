df5a528f1de77726d8ca35bd181300c5459ff60a
Juan Wajnerman <jwajnerman@manas.com.ar>
2011-12-14 15:50:51 -0300

    Force load of DeviseHelper before patching

--- app/helpers/instedd_app_helper.rb
+++ app/helpers/instedd_app_helper.rb
@@ -1,9 +1,9 @@
 module InsteddAppHelper
   def flash_message
     res = nil
-    
+
     keys = { :notice => 'flash_notice', :error => 'flash_error', :alert => 'flash_error' }
-    
+
     keys.each do |key, value|
       if flash[key]
         html_option = { :class => "flash #{value}" }
@@ -15,16 +15,16 @@ module InsteddAppHelper
         end
       end
     end
-    
+
     res
   end
-  
+
   def errors_for(object, options = {})
     unless object.nil?
       if object.errors.any?
          # TODO change on rails 3.1 to ActiveModel::Naming.param_key(object)
         object_name = options[:as].try(:to_s) || ActiveModel::Naming.singular(object)
-          
+
         content_tag :div, :class => "box error_description #{options[:class] || 'w60'}" do
           (content_tag :h2 do
             "#{pluralize(object.errors.count, 'error')} prohibited this #{object_name.humanize} from being saved:"
@@ -39,10 +39,11 @@ module InsteddAppHelper
   end
 end
 
-module DeviseHelper  
+DeviseHelper #Force load of Devise's original module
+module DeviseHelper
   def devise_error_messages!(html_options = {})
     return if resource.errors.full_messages.empty?
-    
+
     (content_tag :div, :class => "box error_description #{html_options[:class] || 'w60'}"  do
       (content_tag :h2, 'The following errors occurred') \
       + \
