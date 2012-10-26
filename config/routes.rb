08d7fdcc74631f368ce205e230ab870675279622
Juan Wajnerman <jwajnerman@manas.com.ar>
2012-10-26 16:39:14 -0300

    Redirect createAccount using the specified locale. Defaults to 'en'.

--- config/routes.rb
+++ config/routes.rb
@@ -1,17 +1,17 @@
 # Copyright (C) 2011-2012, InSTEDD
-# 
+#
 # This file is part of Remindem.
-# 
+#
 # Remindem is free software: you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
 # the Free Software Foundation, either version 3 of the License, or
 # (at your option) any later version.
-# 
+#
 # Remindem is distributed in the hope that it will be useful,
 # but WITHOUT ANY WARRANTY; without even the implied warranty of
 # MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 # GNU General Public License for more details.
-# 
+#
 # You should have received a copy of the GNU General Public License
 # along with Remindem.  If not, see <http://www.gnu.org/licenses/>.
 
@@ -38,7 +38,7 @@ RememberMe::Application.routes.draw do
     match 'help' => 'help#faq'
     match 'community' => 'community#index'
 
-    get  'createAccount', :to => redirect('/users/sign_up')
+    get  'createAccount', :to => redirect('/%{locale}/users/sign_up'), :defaults => { :locale => 'en' }
     get  'discuss',       :to => redirect(RememberMe::Application.config.user_group_url)
     get  'backlog',       :to => redirect(RememberMe::Application.config.backlog_url)
 
