140d796983ceea376f268fc00f00e0560da6b141
Brian J. Cardiff <bcardiff@manas.com.ar>
2016-08-04 14:04:30 -0300

    instedd/telemetry_rails#100
    rename phone_numbers to unique_phone_numbers_by_country
    add unique_phone_numbers_by_project_and_country

--- app/models/telemetry/phone_numbers_collector.rb
+++ app/models/telemetry/phone_numbers_collector.rb
@@ -1,14 +1,44 @@
 module Telemetry::PhoneNumbersCollector
-
   def self.collect_stats(period)
-    query = Subscriber.select(["phone_number, count(*)"])
+    {"counters" => schedules_counters(period).concat(global_counters(period))}
+  end
+
+  def self.schedules_counters(period)
+    subscribers = Subscriber.where("created_at < ?", period.end).select("distinct phone_number, schedule_id")
+    schedule_data = {}
+
+    subscribers.each do |subscriber|
+      schedule_id = subscriber.schedule_id
+      number = subscriber.phone_number.without_protocol
+      country_code = InsteddTelemetry::Util.country_code number
+      if country_code.present?
+        schedule_data[schedule_id] ||= Hash.new(0)
+        schedule_data[schedule_id][country_code] += 1
+      end
+    end
+
+    counters = schedule_data.inject [] do |r, (application_id, numbers_by_country_code)|
+      r.concat(numbers_by_country_code.map do |country_code, count|
+        {
+          "metric" => "unique_phone_numbers_by_project_and_country",
+          "key"    => { "project_id" => application_id, "country_code" => country_code },
+          "value"  => count
+        }
+      end)
+    end
+
+    counters
+  end
+
+  def self.global_counters(period)
+    query = Subscriber.select("distinct phone_number")
                       .where("created_at < ?", period.end)
                       .group("phone_number")
                       .to_sql
-
     results = ActiveRecord::Base.connection.execute query
 
     counts = Hash.new(0)
+
     results.each do |number, count|
       country_code = InsteddTelemetry::Util.country_code(number.without_protocol) rescue nil
       if country_code
@@ -16,16 +46,14 @@ module Telemetry::PhoneNumbersCollector
       end
     end
 
-    {
-      "counters" => counts.map { |code, count|
-        {
-          "metric" => "phone_numbers",
-          "key"    => { "country_code" => code },
-          "value"  => count
-        }
+    counters = counts.map do |code, count|
+      {
+        "metric" => "unique_phone_numbers_by_country",
+        "key"    => { "country_code" => code },
+        "value"  => count
       }
-    }
+    end
+
+    counters
   end
-
-
 end
--- test/unit/telemetry/phone_numbers_collector_test.rb
+++ test/unit/telemetry/phone_numbers_collector_test.rb
@@ -5,11 +5,14 @@ class PhoneNumbersCollectorTest < ActiveSupport::TestCase
   setup do
     set_current_time
     @schedule = RandomSchedule.make
+    @schedule2 = RandomSchedule.make
   end
 
   test "counts numbers by country code for current period" do
     (1..3).each { |i| @schedule.subscribers.make phone_number: "sms://54 11 4444 555#{i}" }
     (1..4).each { |i| @schedule.subscribers.make phone_number: "sms://855 23 686 036#{i}" }
+    (1..2).each { |i| @schedule2.subscribers.make phone_number: "sms://54 11 4444 555#{i}" }
+    (1..3).each { |i| @schedule2.subscribers.make phone_number: "sms://855 23 686 036#{i}" }
 
     period = InsteddTelemetry::Period.current
 
@@ -18,12 +21,32 @@ class PhoneNumbersCollectorTest < ActiveSupport::TestCase
     assert_equal stats, {
       "counters" => [
         {
-        "metric" => "phone_numbers",
+        "metric" => "unique_phone_numbers_by_project_and_country",
+        "key" => { "country_code" => "54", "project_id" => @schedule.id },
+        "value" => 3
+        },
+        {
+        "metric" => "unique_phone_numbers_by_project_and_country",
+        "key" => { "country_code" => "855", "project_id" => @schedule.id },
+        "value" => 4
+        },
+        {
+        "metric" => "unique_phone_numbers_by_project_and_country",
+        "key" => { "country_code" => "54", "project_id" => @schedule2.id },
+        "value" => 2
+        },
+        {
+        "metric" => "unique_phone_numbers_by_project_and_country",
+        "key" => { "country_code" => "855", "project_id" => @schedule2.id },
+        "value" => 3
+        },
+        {
+        "metric" => "unique_phone_numbers_by_country",
         "key" => { "country_code" => "54" },
         "value" => 3
         },
         {
-        "metric" => "phone_numbers",
+        "metric" => "unique_phone_numbers_by_country",
         "key" => { "country_code" => "855" },
         "value" => 4
         }
@@ -41,7 +64,12 @@ class PhoneNumbersCollectorTest < ActiveSupport::TestCase
     assert_equal stats, {
       "counters" => [
         {
-        "metric" => "phone_numbers",
+        "metric" => "unique_phone_numbers_by_project_and_country",
+        "key" => { "country_code" => "54", "project_id" => @schedule.id },
+        "value" => 1
+        },
+        {
+        "metric" => "unique_phone_numbers_by_country",
         "key" => { "country_code" => "54" },
         "value" => 1
         }
@@ -61,7 +89,12 @@ class PhoneNumbersCollectorTest < ActiveSupport::TestCase
     assert_equal Telemetry::PhoneNumbersCollector.collect_stats(p0), {
       "counters" => [
         {
-        "metric" => "phone_numbers",
+        "metric" => "unique_phone_numbers_by_project_and_country",
+        "key" => { "country_code" => "54", "project_id" => @schedule.id },
+        "value" => 2
+        },
+        {
+        "metric" => "unique_phone_numbers_by_country",
         "key" => { "country_code" => "54" },
         "value" => 2
         }
@@ -71,7 +104,12 @@ class PhoneNumbersCollectorTest < ActiveSupport::TestCase
     assert_equal Telemetry::PhoneNumbersCollector.collect_stats(p1), {
       "counters" => [
         {
-        "metric" => "phone_numbers",
+        "metric" => "unique_phone_numbers_by_project_and_country",
+        "key" => { "country_code" => "54", "project_id" => @schedule.id },
+        "value" => 9
+        },
+        {
+        "metric" => "unique_phone_numbers_by_country",
         "key" => { "country_code" => "54" },
         "value" => 9
         }
